fc3342286fc72c39d757a6f37cbd682f54c0abe8
Daniel Rangel <danyrangel2002@gmail.com>
2024-09-05 12:41:24 -0600

    Preparacion a produccion

--- src/app/services/GLOBA.ts
+++ src/app/services/GLOBA.ts
@@ -1,7 +1,7 @@
 export var GLOBAL = {
-    //url: 'https://infinitywares-backend.onrender.com/api/',
-    //socket: 'https://infinitywares-backend.onrender.com'
+    url: 'https://infinitywares-api.onrender.com/api/',
+    socket: 'https://infinitywares-api.onrender.com'
     
-    url : 'http://127.0.0.1:4201/api/',
-    socket: 'http://127.0.0.1:4201'
+    //url : 'http://127.0.0.1:4201/api/',
+    //socket: 'http://127.0.0.1:4201'
 }
